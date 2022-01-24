345fc477efeb2c1361bee12ff986e908a8bbcea8
ylu <ylu@ert.com>
2022-01-24 07:40:52 -0500

    refactor var names

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod http;
 mod server;
 
 fn main() {
-    let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    let http_server = Server::new("127.0.0.1:8080".to_string());
+    http_server.run();
 }
    
 /* request:
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,6 @@
 use std::net::TcpListener;
 use std::io::Read;
 use std::convert::TryFrom;
-// use std::convert::TryInto;
 
 use crate::http::{ParseError, Request};
 
@@ -24,14 +23,14 @@ impl Server {
         loop {
             match listener.accept() {
                 Ok((mut stream, _)) => {
-                    let mut buffer = [0; 1024]; // 1 k bytes
+                    let mut buffer = [0; 1024]; // 1k bytes
 
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
                             match Request::try_from(&buffer[..]) {
-                                Ok(request) => {},
+                                Ok(request) => {println!("Request: {:?}", request); },
                                 Err(e) => println!("Failed to parse a request: {}", e),
                             }
                         }
@@ -43,4 +42,32 @@ impl Server {
         }
     }
 }
+
+
+// test from postman:
+// GET 127.0.0.1:8080/search?a=1088
+// body json:
+// {
+//     "name":"Ying Lu"
+// }
+
+// Finished dev [unoptimized + debuginfo] target(s) in 0.95s 
+// Running `target\debug\http-server.exe`
+// listening on 127.0.0.1:8080
+// Received a request: GET /search?a=1088 HTTP/1.1
+// Content-Type: application/json
+// User-Agent: PostmanRuntime/7.26.8
+// Accept: */*
+// Cache-Control: no-cache
+// Postman-Token: 53afb4a4-d24c-451d-adcd-674d715cf237
+// Host: 127.0.0.1:8080
+// Accept-Encoding: gzip, deflate, br
+// Connection: keep-alive
+// Content-Length: 26
+
+// {
+//     "name":"Ying Lu"
+// }
+// Request: Request { path: "/search", query_string: Some("a=1088"), method: GET }
+
 
