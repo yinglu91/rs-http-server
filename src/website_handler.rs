2d9d88582ff6272ee6e856ca5026e2253691fdd5
ylu <ylu@ert.com>
2022-01-24 23:13:52 -0500

    serve static files

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,6 +1,6 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
-
+use std::fs;
 pub struct WebsiteHandler {
     public_path: String
 }
@@ -11,14 +11,24 @@ impl WebsiteHandler {
             public_path
         }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/about" => Response::new(StatusCode::Ok, self.read_file("about.html")),
                 "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello, there</h1>".to_string())),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
                 _ => Response::new(StatusCode::NotFound, None),
             }
             _ => Response::new(StatusCode::NotFound, None),
